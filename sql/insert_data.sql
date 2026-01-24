52368189688211ffc12fb84e89ceee72359aebf0
Damian Arcipowski <darcipowski@gmail.com>
2026-01-24 13:54:19 +0100

    Removed other employees from db than office worker

--- sql/insert_data.sql
+++ sql/insert_data.sql
@@ -4,6 +4,4 @@ VALUES ('Burek', 'Pies', NULL, '2026-01-13', 3, 'Kundel o brązowym umaszczeniu,
 ('Bela', 'Królik', NULL, '2026-01-02', 1, 'Królik z raną lewego ucha, wymaga dalszego odrobaczania', 'samica', 'dostepne');
 
 INSERT INTO employees (first_name, surname, hire_date, password, login, position) 
-VALUES ('Jan', 'Kowalski', '2026-01-17', '$2y$10$MxLkTp4C38Ie8zCkiNsoX.ZbtyDETWFta7yhG02GH7Kd/X4Rj6uhC', 'jkowalski', 'pracownik_biurowy'),
-('Adam', 'Mickiewicz', '2022-01-17', '$2y$10$MxLkTp4C38Ie8zCkiNsoX.ZbtyDETWFta7yhG02GH7Kd/X4Rj6uhC', 'amickiewicz', 'magazynier'),
-('Stefan', 'Rogowski', '2020-01-17', '$2y$10$MxLkTp4C38Ie8zCkiNsoX.ZbtyDETWFta7yhG02GH7Kd/X4Rj6uhC', 'srogowski', 'weterynarz');
+VALUES ('Jan', 'Kowalski', '2026-01-17', '$2y$10$MxLkTp4C38Ie8zCkiNsoX.ZbtyDETWFta7yhG02GH7Kd/X4Rj6uhC', 'jkowalski', 'pracownik_biurowy');
