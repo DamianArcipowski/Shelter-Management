a71ec7093da16a2499148865766cb4bf0a70c4de
Damian Arcipowski <darcipowski@gmail.com>
2026-01-24 14:01:24 +0100

    Added docker files to deploy the app, updated database connection string

--- backend/connection.php
+++ backend/connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbhost = 'localhost';
-$dbuser = 'admin';
-$dbpassword = 'zaq1@WSX';
-$dbname = 'shelter';
+$dbhost = getenv('DB_HOST');
+$dbuser = getenv('DB_USER');
+$dbpassword = getenv('DB_PASSWORD');
+$dbname = getenv('DB_DATABASE');
 
 $conn = new mysqli($dbhost, $dbuser, $dbpassword, $dbname);
 $conn->set_charset('utf8mb4');
